1075afd2c535ecfd12eba8730306738435ab1229
serhankunt <hserhan006@gmail.com>
2023-10-30 14:56:48 +0300

    Add project files.

---
+++ SOLID/Program.cs
@@ -0,0 +1,13 @@
+﻿using SOLID.OCPGood;
+//Bad Way
+//SalaryCalculator salaryCalculator = new SalaryCalculator();
+//Console.WriteLine($"Low Salary: { salaryCalculator.Calculate(1000, SalaryType.Low)}");
+//Console.WriteLine($"Middle Salary: { salaryCalculator.Calculate(1000, SalaryType.Middle)}");
+//Console.WriteLine($"High Salary: { salaryCalculator.Calculate(1000, SalaryType.High)}");
+
+//Good Way
+
+SalaryCalculator salaryCalculator = new SalaryCalculator();
+Console.WriteLine($"Low Salary: {salaryCalculator.Calculate(10000, new LowSalaryCalculate())}");
+Console.WriteLine($"Middle Salary: {salaryCalculator.Calculate(10000, new MiddleSalaryCalculate())}");
+Console.WriteLine($"High Salary: {salaryCalculator.Calculate(10000, new HighSalaryCalculate())}");
